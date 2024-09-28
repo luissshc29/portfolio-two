2b7a7dfa30ab48e9853613bfbec4d0975e46dc78
Luis Henrique <luishcc2003@gmail.com>
2024-09-27 21:30:24 -0300

    Added flex-wrap to final text on Contacts section

--- src/utils/constants/textVariants.tsx
+++ src/utils/constants/textVariants.tsx
@@ -61,11 +61,11 @@ export const textVariants: TextVariants = {
             sou especializado em criar sites personalizados sob demanda para
             atender às necessidades específicas dos meus clientes. Utilizando
             tecnologias difundidas no mercado web e minha expertise em{" "}
-            <i>TypeScript, React, Next.js</i>, <i>MySQL</i> e <i>GraphQL</i>, entrego soluções
-            web de alta qualidade, responsivas e amigáveis. Minhas habilidades
-            abrangem tanto o desenvolvimento front-end quanto o back-end,
-            garantindo uma experiência web de <u>alta qualidade</u> e{" "}
-            <u>eficiente</u> para os usuários. Estou comprometido em me manter
+            <i>TypeScript, React, Next.js</i>, <i>MySQL</i> e <i>GraphQL</i>,
+            entrego soluções web de alta qualidade, responsivas e amigáveis.
+            Minhas habilidades abrangem tanto o desenvolvimento front-end quanto
+            o back-end, garantindo uma experiência web de <u>alta qualidade</u>{" "}
+            e <u>eficiente</u> para os usuários. Estou comprometido em me manter
             atualizado com as últimas tendências e melhores práticas do setor
             para fornecer <u>soluções inovadoras</u> e <u>eficazes</u>.
           </p>
@@ -75,12 +75,13 @@ export const textVariants: TextVariants = {
             As a <i>web developer</i>, I, <strong>Luis Henrique</strong>,
             specialize in creating customized websites tailored to meet the
             specific needs of my clients. Leveraging popular web technologies
-            and my expertise in <i>TypeScript, React, Next.js</i>, <i>MySQL</i> and <i>GraphQL</i>, I deliver high-quality, responsive, and user-friendly
-            web solutions. My skills encompass both front-end and back-end
-            development, ensuring a <u>high-quality</u> and <u>efficient</u> web
-            experience for users. I am dedicated to staying updated with the
-            latest industry trends and best practices to provide{" "}
-            <u>innovative</u> and <u>effective solutions</u>.
+            and my expertise in <i>TypeScript, React, Next.js</i>, <i>MySQL</i>{" "}
+            and <i>GraphQL</i>, I deliver high-quality, responsive, and
+            user-friendly web solutions. My skills encompass both front-end and
+            back-end development, ensuring a <u>high-quality</u> and{" "}
+            <u>efficient</u> web experience for users. I am dedicated to staying
+            updated with the latest industry trends and best practices to
+            provide <u>innovative</u> and <u>effective solutions</u>.
           </p>
         ),
       },
@@ -466,7 +467,7 @@ export const textVariants: TextVariants = {
       form: {
         text: {
           br: (
-            <div className="flex items-center gap-1">
+            <div className="flex flex-wrap justify-center items-center gap-1">
               <FiInfo className="text-base" />
               Você também pode preencher um formulário,{" "}
               <a
@@ -480,7 +481,7 @@ export const textVariants: TextVariants = {
             </div>
           ),
           us: (
-            <div className="flex items-center gap-1">
+            <div className="flex flex-wrap justify-center items-center gap-1">
               <FiInfo className="text-base" /> You can also fill out a form, by{" "}
               <a
                 href="https://docs.google.com/forms/d/e/1FAIpQLSfr4SLa31PNH489ktl2jPJriFJl3P-se7zCW1X3VzeLZAI8Aw/viewform"
