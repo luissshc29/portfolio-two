ce3b720485ed26d5636bfba7f1d066646afdf75e
Luis Henrique <luishcc2003@gmail.com>
2024-10-15 21:38:47 -0300

    Modified Warning toast description text

--- src/utils/constants/textVariants.tsx
+++ src/utils/constants/textVariants.tsx
@@ -46,8 +46,8 @@ export const textVariants: TextVariants = {
           us: "It seems that you came from ",
         },
         description: {
-          br: `Caso esteja utilizando o navegador nativo do [APP], é recomendado que você troque para um <u>navegador externo</u> (Ex.: Safari, Google Chrome, ...) para prosseguir na página.<br/><br/>Caso decida continuar por aqui, podem ocorrer bugs, lentidões e/ou erros em algumas funcionalidades, devido às limitações desses navegadores nativos.`,
-          us: `If you are using [APP]'s native browser, it is recommended that you switch to an <u>external browser</u> (E.g.: Safari, Google Chrome, ...) to continue on the page.<br/><br/>If you decide to go on, bugs, slowdowns and/or errors in some features may occur, due to the limitations of these native browsers.`,
+          br: `Caso esteja utilizando o navegador nativo do [APP], é recomendado que você troque para um <u>navegador externo</u> (Ex.: Safari, Google Chrome, ...) para prosseguir na página.<br/><br/>Caso decida continuar por aqui, podem ocorrer bugs, lentidões e/ou erros em algumas funcionalidades, devido às limitações de navegadores nativos.`,
+          us: `If you are using [APP]'s native browser, it is recommended that you switch to an <u>external browser</u> (E.g.: Safari, Google Chrome, ...) to continue on the page.<br/><br/>If you decide to go on, bugs, slowdowns and/or errors in some features may occur, due to the limitations of native browsers.`,
         },
       },
     },
