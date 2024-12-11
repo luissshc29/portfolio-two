310ec1c178bb7656a519433c4c3bd2c60ab5468e
Luis Henrique <luishcc2003@gmail.com>
2024-12-10 21:55:05 -0300

    Fixed "underline" font-size on Title component

--- src/app/components/common/Title.tsx
+++ src/app/components/common/Title.tsx
@@ -4,13 +4,13 @@ import React, { useEffect, useRef, useState } from "react";
 import { useInView } from "react-intersection-observer";
 
 export default function Title({
-  variant = 'text-only',
-  bgIcon = '',
+  variant = "text-only",
+  bgIcon = "",
   mainText,
   bgText = "",
 }: {
-  variant?: 'text-only' | 'with-bg-icon'
-  bgIcon?: React.ReactNode
+  variant?: "text-only" | "with-bg-icon";
+  bgIcon?: React.ReactNode;
   mainText: string;
   bgText?: string;
 }) {
@@ -32,41 +32,36 @@ export default function Title({
   const { ref, inView } = useInView();
 
   useEffect(() => {
-    if (variant === 'text-only') {
+    if (variant === "text-only") {
       setTitle("");
       textIndex.current = 0;
       if (inView) {
         typing();
       }
     }
-
   }, [mainText, inView]);
 
-  if (variant === 'text-only') {
+  if (variant === "text-only") {
     return (
       <div
-        className="relative flex justify-center items-center w-full text-center"
+        className="relative flex w-full items-center justify-center text-center"
         ref={ref}
       >
-        <h1 className="top-0 z-[-2] absolute opacity-20 font-title-bg text-[#838383] text-5xl md:text-7xl dark:text-[#9b9b9b]">
+        <h1 className="absolute top-0 z-[-2] font-title-bg text-5xl text-[#838383] opacity-20 dark:text-[#9b9b9b] md:text-7xl">
           {bgText}
         </h1>
         <h2 className="font-title text-4xl md:text-5xl">{title}</h2>
-        <span className="animate-blink">_</span>
+        <span className="animate-blink text-4xl md:text-5xl">_</span>
       </div>
     );
   } else {
     return (
-      <div
-        className="relative flex justify-center items-center text-center"
-      >
-        <h1 className="mx-auto top-[-100%] z-[-2] absolute font-title-bg text-[#adadad] text-3xl md:text-4xl dark:text-[#363636]">
+      <div className="relative flex items-center justify-center text-center">
+        <h1 className="absolute top-[-100%] z-[-2] mx-auto font-title-bg text-3xl text-[#adadad] dark:text-[#363636] md:text-4xl">
           {bgIcon}
         </h1>
         <h2 className="font-title text-base md:text-lg">{mainText}</h2>
       </div>
     );
   }
-
-
 }
