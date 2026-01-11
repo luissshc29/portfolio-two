49dd8f8a416536813e347463db3ee0141c14e4e2
Luis Henrique <luishcc2003@gmail.com>
2026-01-11 18:07:57 -0300

    Minor fix.

--- src/app/components/unique/Footer.tsx
+++ src/app/components/unique/Footer.tsx
@@ -12,16 +12,18 @@ export default function Footer() {
   const { resolvedTheme } = useTheme();
 
   return (
-    <footer className="z-[1000] mx-auto mb-4 flex w-screen flex-col-reverse items-center justify-evenly gap-3 text-xs text-neutral-700 dark:text-neutral-400 md:flex-row md:text-sm">
-      <div className="flex w-fit items-center gap-2 text-neutral-600 opacity-70 dark:text-neutral-200 dark:opacity-70 md:opacity-60 md:dark:opacity-40">
+    <footer className="z-[1000] flex md:flex-row flex-col-reverse justify-evenly items-center gap-3 mx-auto mb-4 w-screen text-neutral-700 dark:text-neutral-400 text-xs md:text-sm">
+      <div className="flex items-center gap-2 opacity-70 md:dark:opacity-40 md:opacity-60 dark:opacity-70 w-fit text-neutral-600 dark:text-neutral-200">
         <p>{language === "br" ? "Feito com" : "Made with"}</p>
         <Separator
           orientation="vertical"
-          className="h-5 w-[1px] bg-neutral-600 opacity-100 dark:bg-neutral-200"
+          className="bg-neutral-600 dark:bg-neutral-200 opacity-100 w-[1px] h-5"
         />
         <div className="flex items-center gap-2">
           {stacks
-            .filter((item) => [4, 6, 10].includes(item.id))
+            .filter((item) =>
+              ["tailwindcss", "typescript", "nextjs"].includes(item.id),
+            )
             .map((s) => (
               <a
                 key={s.id}
